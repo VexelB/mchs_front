b71efe90de7eacd04d6465e2ada9f9f5ca40e098
VexelB <vexelb@gmail.com>
2021-03-22 15:01:53 +0300

    fixes

--- indexadmin.js
+++ indexadmin.js
@@ -9,15 +9,40 @@ let get = (x, i) => {
 }
 
 // init
-const ws = new WebSocket ("wss://localhost:5353")
+const ws = new WebSocket("wss://localhost:5353")
+let head = {}
 let reqbody = {};
 let assoc = {};
 let table = '';
-let fields = {}
+let fields = {};
+let page = 0;
+let init = () => {
+    document.querySelectorAll('.row').forEach((x) => {
+        x.addEventListener('click', () => {
+            reqbody.action = "change";
+            let temp = document.querySelectorAll(`#${x.parentNode.parentNode.id} #${x.id}`)
+            head.fields = {}
+            for (let i in temp) {
+                if (temp[i].parentNode) {
+                    head.fields[fields[table][i]] = temp[i].innerText
+                }   
+            }
+            document.querySelector('#myModal #modaldata').innerHTML = ''
+            for (let i in fields[table]) {
+                document.querySelector('#myModal #modaldata').innerHTML += `<div id="div${fields[table][i]}">${assoc[fields[table][i]]}: <input id = "input${fields[table][i]}" value="${head.fields[fields[table][i]]}"></div>`;
+                if (fields[table][i] == 'id') {
+                    reqbody.oldid = head.fields[fields[table][i]];
+                }
+            }
+            document.querySelector('#myModal').style.display = "block";
+        })
+    })
+}
 // table switch lictener
 document.querySelectorAll('.tables').forEach((x) => {
     x.addEventListener('click', (x) => {
-        get(x.target.id, 1)
+        page = 1
+        get(x.target.id, page)
         document.querySelector('#shapbtns').style.display = "inline-block";
         table = x.target.id;
         document.querySelectorAll('.data').forEach((e) => {
@@ -36,13 +61,12 @@ document.querySelectorAll('.tables').forEach((x) => {
                 e.style.display = "none"
             }
         })
-        setTimeout(()=> {document.querySelector(`#maindata #${x.target.id} div`).style.display = "block";}, 200);
     })
 })
 
 ws.onopen = () => {
-    get('people', 0)
-    get('access', 0)
+    get('people', page)
+    get('access', page)
 }
 
 // message recieve
@@ -62,26 +86,29 @@ ws.onmessage = (d) => {
         document.querySelectorAll('#pages .page').forEach((x) => {
             x.addEventListener('click', (q) => {
                 get(table, q.target.innerHTML)
+                page = q.target.innerHTML
             })
         })
     }
     else if (data.action == "rows") {
+        console.log(data.content)
         fields[data.table] = []
         for (let i in data.content[0]) {
             fields[data.table].push(i);
         }
         document.querySelector(`#maindata `).innerHTML = ``;
+        for (let j in data.content[0]) {
+            document.querySelector(`#maindata `).innerHTML += `<div class = "table" id="${data.table}${j}"><div class = "tablehead">${assoc[j]}</div></div>`
+            
+        }
         for (let i = 1; i <= Math.ceil(data.content.length / 50); i++) {
-            for (let j in data.content[0]) {
-                document.querySelector(`#maindata `).innerHTML += `<div class = "table" id="${data.table}${j}"><div class = "tablehead">${assoc[j]}</div></div>`
-                
-            }
             for (let j = 0; j < 50; j++) {              
                 for (let q in data.content[j+50*(i-1)]) {
                     document.querySelector(`#maindata #${data.table}${q}`).innerHTML += `<div class="row" id="row${data.content[j+50*(i-1)].id}">${data.content[j+50*(i-1)][q]}</div>  `
                 }
             }
         }
+        init()
     }
 };
 
@@ -95,6 +122,7 @@ document.querySelector("#addbtn").addEventListener('click', () => {
             document.querySelector('#myModal #modaldata').innerHTML += `<div id="div${fields[table][i]}">${assoc[fields[table][i]]}: <input id = "input${fields[table][i]}" value=""></div>`;
         }
     }
+    reqbody.action = "put";
     document.querySelector('#myModal').style.display = "block"
 })
 // add and close addwindow
@@ -109,29 +137,39 @@ document.querySelectorAll('#addclose').forEach((x) => {
                 reqbody.fields[x.id.slice(3)] = '-'
             }
         })
-        reqbody.action = "put";
         reqbody.table = table;
-        reqbody.sql = `INSERT into ${table} values ((select count from ${reqbody.table} + 1) `
-        for (let i in reqbody.fields) {
-        reqbody.sql += `'${reqbody.fields[i]}',`
+        if (reqbody.action == "put") {
+            reqbody.sql = `INSERT into ${table} values ((select count(*) from ${reqbody.table}) + 1, `
+            for (let i in reqbody.fields) {
+                reqbody.sql += `'${reqbody.fields[i]}',`
+            }
+            reqbody.sql = reqbody.sql.slice(0,reqbody.sql.length-1) + ');'
+        } else if (reqbody.action == "change") {
+            document.querySelectorAll('#modaldata div').forEach((x) => {
+                if (x.childNodes[1].value != '') {
+                    reqbody.fields[x.id.slice(3)] = x.childNodes[1].value;
+                } else {
+                    reqbody.fields[x.id.slice(3)] = '-';
+                }
+            })
+            reqbody.sql = `update ${reqbody.table} set `;
+            for (let i in reqbody.fields) {
+                reqbody.sql += `${i} = '${reqbody.fields[i]}',`;
+            }
+            reqbody.sql = reqbody.sql.slice(0, reqbody.sql.length-1) + `where id = ${reqbody.oldid}`;
         }
-        reqbody.sql = reqbody.sql.slice(0,reqbody.sql.length-1) + ');'
         ws.send(JSON.stringify(reqbody))
         reqbody = {}
+        setTimeout(() => {
+            get(table, page)
+        }, 100) 
     })
 })
 // close addwindow listener
 document.querySelectorAll('#close').forEach((x) => {
     x.addEventListener('click', () => {
         x.parentNode.parentNode.parentNode.style.display = "none";
-        if (video) {
-            video.srcObject.getTracks().forEach((track) => {
-                track.stop();
-                oldcode = {data: ''}
-                document.getElementById("loadingMessage").hidden = false;
-            })
-        }
-        document.querySelector('#inputpupil').value = ''
+        document.querySelector('#modaldata').value = ''
     })
 
 })
